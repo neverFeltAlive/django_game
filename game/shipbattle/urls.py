004872d29680742bad78cbe2f50b51a23ccd5e22
neverFeltAlive <53705924+neverFeltAlive@users.noreply.github.com>
2022-03-15 16:00:46 +0300

    Minor Update: basic structure created

--- game/shipbattle/urls.py
+++ game/shipbattle/urls.py
@@ -6,4 +6,14 @@ urlpatterns = [
     path('login', views.login, name='login'),           # https://website/login
     path('sign-up', views.sign_up, name='sign_up'),     # https://website/sign-up
     path('play', views.play, name='play'),              # https://website/play
+    path('about', views.about, name='about'),              # https://website/about
 ]
+
+handler404 = views.page_not_fount               # page not found
+
+# TODO: create server error handlers
+'''
+handler400                                      # unable to process request
+handler403 = views.access_denied                # access denied
+handler500 = views.server_error                 # server error
+'''
--- game/shipbattle/views.py
+++ game/shipbattle/views.py
@@ -1,18 +1,69 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseForbidden, HttpResponseServerError, HttpResponseNotFound
+
+menu = [
+    {'name': 'Home', 'url': 'home'},
+    {'name': 'About', 'url': 'about'},
+    {'name': 'Play', 'url': 'play'},
+]
 
 
 def index(request):
-    return HttpResponse('Index')
+    context = {
+        'title': 'Home',
+        'menu': menu,
+        'active_link': 'Home',
+    }
+    return render(request, 'shipbattle/index.html', context=context)
 
 
 def login(request):
-    return HttpResponse('Login')
+    context = {
+        'title': 'Login',
+    }
+    return render(request, 'shipbattle/login.html', context=context)
 
 
 def sign_up(request):
-    return HttpResponse('Sign Up')
+    context = {
+        'title': 'Sign Up',
+    }
+    return render(request, 'shipbattle/sign_up.html', context=context)
 
 
 def play(request):
-    return HttpResponse('Play')
+    context = {
+        'title': 'Play',
+        'menu': menu,
+        'active_link': 'Play',
+    }
+    return render(request, 'shipbattle/play.html', context=context)
+
+
+def about(request):
+    context = {
+        'title': 'About',
+        'menu': menu,
+        'active_link': 'About',
+    }
+    return render(request, 'shipbattle/about.html', context=context)
+
+
+# Error Handlers
+
+def page_not_fount(request, exception):
+    return HttpResponseNotFound('404')
+
+
+def access_denied(request, exception):
+    return HttpResponseForbidden('403')
+
+
+def server_error(request, exception):
+    return HttpResponseServerError('500')
+
+
+# Custom Functions
+
+def mark_active():
+    pass
